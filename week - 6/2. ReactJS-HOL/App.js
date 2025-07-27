6689165ed60e3e5a08273aa5d648a2ffad6b407e
Ryuvasri <119523422+Ryuvasri@users.noreply.github.com>
2025-07-27 18:59:04 +0530

    Add files via upload

---
+++ week - 6/2. ReactJS-HOL/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Home from './component/home';
+import About from './component/about';
+import Contact from './component/Contact';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <About />
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
